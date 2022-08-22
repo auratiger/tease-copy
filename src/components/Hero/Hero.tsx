5af418121d1add054db26dfa90f2d8295a8e202d
Georgi Georgiev <gegeorgiev@vmware.com>
2022-08-22 16:33:53 +0300

    fixed hero slide transitions

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -4,220 +4,237 @@ import { flex, hideScroll } from '@mixins/mixins';
 import { slideshowItems } from '@constants/data/hero-slideshow';
 import { Button } from '@components/Button';
 import AnimationCascade from '@components/AnimationCascade';
-import { useEffect } from 'react';
 import useInterval from '@hooks/useInterval';
-import { graphql, Link, useStaticQuery } from 'gatsby';
-import { GatsbyImage, getImage } from 'gatsby-plugin-image';
+import { graphql, useStaticQuery } from 'gatsby';
+import { GatsbyImage, getImage, IGatsbyImageData } from 'gatsby-plugin-image';
 
 const tolerance: number = 150;
-const nextSlideDelay: number = 10000;
+const sensitivity: number = 10;
+const nextSlideDelay: number = 15000;
+const slidesCount: number = slideshowItems.length;
 
 const Hero = () => {
-  const scrollRef = useRef<HTMLElement>(null);
-  const [startX, setStartX] = useState<number>(0);
-  const [isDown, setIsDown] = useState<boolean>(false);
-  const [move, setMove] = useState<number>(0);
-  const [slideIndex, setSlideIndex] = useState<number>(0);
+   const scrollRef = useRef<HTMLElement>(null);
+   const [startX, setStartX] = useState<number>(0);
+   const [isDown, setIsDown] = useState<boolean>(false);
+   const [slideIndex, setSlideIndex] = useState<number>(0);
 
-  const {
-    allFile: { edges },
-  } = useStaticQuery(query);
+   const {
+      allFile: { edges },
+   } = useStaticQuery(query);
 
-  useEffect(() => {
-    if (scrollRef.current) {
-      setMove(scrollRef.current.scrollWidth / slideshowItems.length);
-    }
-  }, [scrollRef]);
+   useInterval(
+      () => {
+         if (scrollRef.current) {
+            const slidePosition: number = calculateNextSlidePosition(
+               scrollRef.current
+            );
 
-  useInterval(
-    () => {
+            scrollRef.current.scrollLeft = slidePosition;
+            setSlideIndex(
+               calculateNextSlideIndex(
+                  slidePosition,
+                  scrollRef.current.scrollWidth
+               )
+            );
+         }
+      },
+      isDown ? null : nextSlideDelay
+   );
+
+   const calculateNextSlidePosition = (scrollRef: any, dir = 1): number => {
+      const slideWidth: number = scrollRef.scrollWidth / slidesCount;
+
+      const nextSlidePosition: number = scrollRef.scrollLeft + slideWidth * dir;
+
+      const nextSlidePositionBuffer: number = nextSlidePosition + sensitivity;
+      if (nextSlidePositionBuffer > scrollRef.scrollWidth) {
+         return 0;
+      } else if (nextSlidePositionBuffer < 0) {
+         return scrollRef.scrollWidth - slideWidth;
+      }
+
+      return nextSlidePosition;
+   };
+
+   const calculateNextSlideIndex = (
+      slidePosition: number,
+      scrollWidth: number
+   ): number => {
+      return Math.ceil(
+         (slidePosition - sensitivity) / (scrollWidth / slidesCount)
+      );
+   };
+
+   // events
+   const onMouseDown = (e: any): void => {
+      e.persist();
+      setIsDown(true);
+
       if (scrollRef.current) {
-        const { scrollWidth, scrollLeft } = scrollRef.current;
-        let moveTo: number = scrollLeft + move;
-
-        if (moveTo >= scrollWidth) moveTo = 0;
-
-        scrollRef.current.scrollLeft = moveTo;
-        setSlideIndex((prev: number) => (prev + 1) % slideshowItems.length);
+         setStartX(e.pageX - scrollRef.current.offsetLeft);
+         scrollRef.current.style.cursor = 'grabbing';
       }
-    },
-    isDown ? null : nextSlideDelay
-  );
+   };
 
-  // events
-  const onMouseDown = (e: any): void => {
-    e.persist();
-    setIsDown(true);
+   const onMouseUp = (e: any): void => {
+      if (!isDown) return;
 
-    if (scrollRef.current) {
-      setStartX(e.pageX - scrollRef.current.offsetLeft);
-      scrollRef.current.style.cursor = 'grabbing';
-    }
-  };
+      if (!scrollRef.current) return;
 
-  const onMouseUp = (e: any): void => {
-    if (!isDown) return;
+      scrollRef.current.style.cursor = 'grab';
 
-    if (!scrollRef.current) return;
+      const { offsetLeft, scrollWidth } = scrollRef.current;
 
-    scrollRef.current.style.cursor = 'grab';
+      const endX: number = e.pageX - offsetLeft;
+      const distance: number = startX - endX; // distance from start to finish
 
-    const { offsetLeft, scrollWidth, scrollLeft } = scrollRef.current;
-    const endX: number = e.pageX - offsetLeft;
-    const distance: number = startX - endX; // distance from start to finish
+      if (Math.abs(distance) > tolerance) {
+         const xDirection: number = Math.sign(distance);
+         const slidePosition: number = calculateNextSlidePosition(
+            scrollRef.current,
+            xDirection
+         );
 
-    if (Math.abs(distance) > tolerance) {
-      const xDirection: number = Math.sign(distance);
-      let moveTo: number = scrollLeft + move * xDirection;
-      let newIndex: number = (slideIndex + xDirection) % slideshowItems.length;
-
-      if (moveTo >= scrollWidth) {
-        moveTo = 0;
-        newIndex = 0;
-      } else if (moveTo < 0) {
-        moveTo = scrollWidth;
-        newIndex = slideshowItems.length - 1;
+         scrollRef.current.scrollLeft = slidePosition;
+         setSlideIndex(calculateNextSlideIndex(slidePosition, scrollWidth));
       }
+      setIsDown(false);
+   };
 
-      scrollRef.current.scrollLeft = moveTo;
-      setSlideIndex(newIndex);
-    }
-    setIsDown(false);
-  };
+   // render elements
+   const showItems = () => {
+      return slideshowItems.map((item: any, index: number) => {
+         const image: IGatsbyImageData = getImage(edges[index].node);
+         const isActiveSlide: boolean = index === slideIndex;
 
-  // render elements
-  const showItems = () => {
-    return slideshowItems.map((item: any, index: number) => {
-      const image = getImage(edges[index].node);
-      const isActiveSlide: boolean = index === slideIndex;
+         return (
+            <section key={index} className={`slide`}>
+               <GatsbyImage image={image} className={'hero-image'} alt="hero" />
+               <AnimationCascade active={isActiveSlide} className="title-box">
+                  <h2>{item.title}</h2>
+                  <h3>{item.description}</h3>
+                  <Button url={item.url} text={item.button.text} />
+               </AnimationCascade>
+            </section>
+         );
+      });
+   };
 
-      return (
-        <section key={index} className={`slide`}>
-          <GatsbyImage image={image} className={'hero-image'} alt="hero" />
-          <AnimationCascade active={isActiveSlide} className="title-box">
-            <h2>{item.title}</h2>
-            <h3>{item.description}</h3>
-            <Button url={item.url} text={item.button.text} />
-          </AnimationCascade>
-        </section>
-      );
-    });
-  };
+   const timelineBoxes = () => {
+      return slideshowItems.map((_: any, index: number) => {
+         const isActiveSlide: boolean = index === slideIndex;
+         return (
+            <section className="timeline-box" key={index}>
+               <div className={`background ${isActiveSlide && 'active'}`}></div>
+            </section>
+         );
+      });
+   };
 
-  const timelineBoxes = () => {
-    return slideshowItems.map((_: any, index: number) => {
-      return (
-        <section className="timeline-box" key={index}>
-          <div
-            className={`background ${index === slideIndex && 'active'}`}
-          ></div>
-        </section>
-      );
-    });
-  };
-
-  return (
-    <Wrapper>
-      <div
-        ref={scrollRef}
-        className="slideshow"
-        onMouseDown={onMouseDown}
-        onMouseUp={onMouseUp}
-        onMouseLeave={onMouseUp}
-      >
-        {showItems()}
-      </div>
-      <TimelineContainer>{timelineBoxes()}</TimelineContainer>
-    </Wrapper>
-  );
+   return (
+      <Wrapper>
+         <div
+            ref={scrollRef}
+            className="slideshow"
+            onMouseDown={onMouseDown}
+            onMouseUp={onMouseUp}
+            onMouseLeave={onMouseUp}
+         >
+            {showItems()}
+         </div>
+         <TimelineContainer>{timelineBoxes()}</TimelineContainer>
+      </Wrapper>
+   );
 };
 
 const TimelineContainer = styled.div`
-  ${flex()};
-  gap: 1rem;
-  position: absolute;
-  bottom: 0;
+   ${flex()};
+   gap: 1rem;
+   position: absolute;
+   bottom: 0;
 
-  .timeline-box {
-    width: 8rem;
-    height: 0.5rem;
-    background: rgb(219, 200, 184);
-  }
+   .timeline-box {
+      width: 8rem;
+      height: 0.5rem;
+      background: rgb(219, 200, 184);
+   }
 
-  .background {
-    height: 100%;
-    width: 0%;
-    background: rgb(232, 230, 227);
-    animation-duration: ${nextSlideDelay}ms;
-    animation-timing-function: linear;
-  }
-
-  .background.active {
-    width: 100%;
-    animation-name: loader;
-  }
-
-  @keyframes loader {
-    0% {
+   .background {
+      height: 100%;
       width: 0%;
-    }
-    100% {
+      background: rgb(232, 230, 227);
+      animation-duration: ${nextSlideDelay}ms;
+      animation-timing-function: linear;
+   }
+
+   .background.active {
       width: 100%;
-    }
-  }
+      animation-name: loader;
+   }
+
+   @keyframes loader {
+      0% {
+         width: 0%;
+      }
+      100% {
+         width: 100%;
+      }
+   }
 `;
 
 const Wrapper = styled.article`
-  width: 100%;
-  height: 40rem;
-  position: relative;
-  overflow-x: hidden;
-  ${flex()};
+   width: 100%;
+   height: 40rem;
+   position: relative;
+   overflow-x: hidden;
+   ${flex()};
 
-  .slideshow {
-    display: flex;
-    width: 100%;
-    height: 100%;
-    cursor: pointer;
-    overflow: hidden;
-    ${hideScroll}
-  }
+   .slideshow {
+      display: flex;
+      width: 100%;
+      height: 100%;
+      scroll-snap-type: x proximity;
+      cursor: pointer;
+      overflow: hidden;
+      ${hideScroll}
+   }
 
-  .slide {
-    width: 100%;
-    height: 100%;
-    flex: none;
-    padding: 8rem 3rem;
-    color: white;
-    position: relative;
-  }
+   .slide {
+      width: 100%;
+      height: 100%;
+      flex: none;
+      padding: 8rem 3rem;
+      color: white;
+      position: relative;
+   }
 
-  .hero-image {
-    pointer-events: none;
-    position: absolute;
-    inset: 0;
-    z-index: -1;
-  }
+   .hero-image {
+      pointer-events: none;
+      position: absolute;
+      inset: 0;
+      z-index: -1;
+   }
 
-  .title-box {
-    ${flex({ dir: 'column', align: 'flex-start' })}
-  }
+   .title-box {
+      ${flex({ dir: 'column', align: 'flex-start' })}
+   }
 `;
 
 export const query = graphql`
-  query {
-    allFile(filter: { relativeDirectory: { eq: "hero" } }) {
-      edges {
-        node {
-          name
-          childImageSharp {
-            gatsbyImageData(layout: FULL_WIDTH, placeholder: BLURRED)
-          }
-        }
+   query {
+      allFile(filter: { relativeDirectory: { eq: "hero" } }) {
+         edges {
+            node {
+               name
+               childImageSharp {
+                  gatsbyImageData(layout: FULL_WIDTH, placeholder: BLURRED)
+               }
+            }
+         }
+         totalCount
       }
-      totalCount
-    }
-  }
+   }
 `;
 
 export default Hero;
